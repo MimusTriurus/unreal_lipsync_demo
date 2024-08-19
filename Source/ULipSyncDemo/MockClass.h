dcd9660e9040fe1aa8cbc225638ac596bc788374
SSS <stendhal.syndrome.studio@gmail.com>
2024-08-19 20:29:36 +0100

    init repository

---
+++ Source/ULipSyncDemo/MockClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ULIPSYNCDEMO_API MockClass
+{
+public:
+	MockClass();
+	~MockClass();
+};
